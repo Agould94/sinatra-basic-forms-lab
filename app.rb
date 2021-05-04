cef5201e4c11d8b33a281154ef1f8c2280d404db
Alexander Gould <alexander.gould94@gmail.com>
2021-05-03 18:38:25 -0700

    commit tree 3eab034c22693114275a5c1e03ff9baeab7ef763

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    set :views, settings.root + '/4:26-4:30/sinatra-basic-forms-lab/views'
+    get '/' do
+        erb :index
+    end
 
+    get '/new' do
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        erb :display_puppy
+    end
 end
